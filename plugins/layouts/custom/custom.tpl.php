f94b75789fe57400e1fb3caa405177d08222073b
Luis Elizondo <lelizondo@gmail.com>
2013-08-23 16:17:37 -0500

    Removed code from custom.tpl.php that should be inside a preprocess function

--- plugins/layouts/custom/custom.tpl.php
+++ plugins/layouts/custom/custom.tpl.php
@@ -12,51 +12,17 @@
  *   - $content['left']: Content in the left column.
  *   - $content['middle']: Content in the middle column.
  *   - $content['right']: Content in the right column.
- */
-  if(arg(0) == "node" && is_numeric(arg(1))) {
-    $node = node_load(arg(1));
-    $counter = 1;
-  }
-
-  function bootstrap_columns($cols) {
-    $width = 12/$cols;
-    $arr = array(
-      "col-xs-" . $width,
-      "col-sm-" . $width,
-      "col-md-" . $width,
-      "col-lg-" . $width
-    );
-
-    return implode(" ", $arr);
-  }
-
-  /**
-   * Set inline styles
-   */
-  function setStyles($section) {
-    // Default to white
-    $color = isset($section["backgroundColor"]) ? $section["backgroundColor"] : "#ffffff";
-
-    // Check if the user entered the color with # or without
-    if(strpos($color, "#") === false) {
-      $styles = "background-color: #" . $color;
-    }
-    else {
-      $styles = "background-color: " . $color;  
-    }
-
-    return $styles;
-  }
+ */ 
 ?>
 
 <div class="panel-container">
-  <?php foreach($node->panels_node["panels_onepage"]["sections"] as $key => $section): ?>
-    <div id="container-outter-section-<?php print $key; ?>" class="container-outter container-outter-section-<?php print $key; ?>" style="<?php print setStyles($section); ?>">
+  <?php foreach($sections as $key => $section): ?>
+    <div id="container-outter-section-<?php print $key; ?>" class="container-outter container-outter-section-<?php print $key; ?>" style="<?php print $section["style"]; ?>">
       <div class="container section-container">
         <div id="<?php print 'section-' . $key; ?>" class="row">
           <?php $cols = $section["cols"]; ?>
           <?php for($i=1; $i <= $cols; $i++): ?>
-            <div id="col-<?php print $counter; ?>" class="<?php print bootstrap_columns($cols); ?>">
+            <div id="col-<?php print $counter; ?>" class="<?php print $section["columns-class"]; ?>">
               <?php print $content["col-" . $counter]; ?>
               <?php $counter++; ?>
             </div>
